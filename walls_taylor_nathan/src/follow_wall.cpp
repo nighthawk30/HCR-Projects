7c02b08ba90d8f6298b6bf2287652c7a9d750336
nighthawk30 <nathan4taylor@gmail.com>
2021-03-10 20:50:01 -0700

    predefined qtable setup and outline complete

--- walls_taylor_nathan/src/follow_wall.cpp
+++ walls_taylor_nathan/src/follow_wall.cpp
@@ -6,14 +6,19 @@
 #include "geometry_msgs/Pose2D.h"
 #include "sensor_msgs/LaserScan.h"
 #include "std_msgs/String.h"
+#include <string>
+#include <map>
 #include <cstdlib>
 
 #include <cmath>
 ros::Publisher pub;
 ros::Subscriber sub;
 
+/*
 void move(double distance);
 void turn(double angle);//ccw
+*/
+void moveTurn(double distance, double ang_degrees);//ccw+
 
 void poseCallback(const sensor_msgs::LaserScan::ConstPtr& scan)
 {
@@ -32,27 +37,72 @@ int main(int argc, char **argv)
   ros::Rate rate(10);
   pub = node.advertise<geometry_msgs::Pose2D>("/triton_lidar/vel_cmd", 10);
   sub = node.subscribe<sensor_msgs::LaserScan>("/scan", 1000, poseCallback);
-  
   //init time
-  ros::Duration(10.0).sleep();
+  ros::Duration(2.0).sleep();
+  //
+  ROS_INFO("Get RRRReeadddy toooooo Ruuummmmmmblllleee");
+  //Setup
+  moveTurn(5,0);
+  moveTurn(0,-90);
+  //
 
-  ROS_INFO("Mooooooved");
-  move(10);
-  
-  
-  //Do normal stuff
+
+  //DEFINE STATE ACTION PAIRS
+  std::map<std::string, double> qtable;//state, action(drive = .1, angle)
+  qtable["close"] = -5;
+  qtable["med"] = 0;
+  qtable["far"] = 5;
+
+
+  double etime = ros::Time::now() + ros::Duration(10.0);
+  while (ros::Time::now() < etime)
+    {
+      //check state
+
+      //choose action
+    }
+
   ros::spin();
 }
 
+//need to be able to do both at once
+void moveTurn(double distance, double ang_degrees)
+{
+  double angular_speed = .2;
+  if (ang_degrees <= 0)
+    angular_speed *= -1;
+  double speed = .5;
+  double PI = 3.141592653589693;
+  double ang_rad = ang_degrees * PI/180;
+  double cdist = 0;//current distance
+  double cang = 0;//current angle
+  geometry_msgs::Pose2D stop;
+
+  double start = ros::Time::now().toSec();
+  while (cang < ang_rad || cdist < distance)
+    {
+      cang = angular_speed * (ros::Time::now().toSec() - start);
+      cdist = speed * (ros::Time::now().toSec() - start);
+      //set msg
+      geometry_msgs::Pose2D msg;
+      if (abs(cang) < abs(ang_rad))
+	msg.theta = angular_speed;
+      if (cdist < distance)
+	msg.x = speed;
+      //print message
+      pub.publish(msg);
+      //give up
+      ros::spinOnce();
+    }
+  pub.publish(stop);
+}
+/*
 void turn(double angle)
 {
   //ccw is positive
   double angular_speed = 1;
   double PI = 3.141592653589693;
-
-  //The rotation angle is off the real angle: cf scales the rotation to correct
-  double cf = .02/105*abs(angle)+.985-.02/105*30+.002;//linear apprx of correction
-  double radians = angle * PI/180*cf;
+  double radians = angle * PI/180;
 
   geometry_msgs::Pose2D msg;
   geometry_msgs::Pose2D stop;//stop the rotation
@@ -89,3 +139,4 @@ void move(double distance)
     }
   pub.publish(stop);
 }
+*/
